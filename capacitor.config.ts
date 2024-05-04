f56cb1b08dda524982ee907f5ee3e2e36b392bdc
Rafael Ortiz <rortiz@inkremental.co>
2024-05-04 14:39:58 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'colombiapp',
+  webDir: 'www'
+};
+
+export default config;
