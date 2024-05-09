3d5bb2d08d7e3f951fc09ca3a1c4efbebf5a5701
Rafael Ortiz <rortiz@inkremental.co>
2024-05-09 08:40:59 -0500

    Add desktop view message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonMenu, IonRow, IonCol, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, informationOutline, informationCircleOutline, homeOutline, homeSharp, informationCircleSharp, layersOutline, gridOutline, layersSharp, gridSharp, mapOutline, mapSharp, businessOutline, businessSharp } from 'ionicons/icons';
 import { MenuComponent } from './menu/menu.component';
@@ -14,11 +14,20 @@ register();
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, MenuComponent],
+  imports: [RouterLink, IonHeader, IonRow, IonCol, IonToolbar, IonTitle, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, MenuComponent],
 })
 export class AppComponent {
 
+  isDesktop: boolean = false;
+
   constructor() {
     addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, informationOutline, informationCircleOutline, homeOutline, homeSharp, informationCircleSharp, layersOutline, gridOutline, layersSharp, gridSharp, mapSharp, mapOutline, businessOutline, businessSharp });
+    this.onResize({ target: window });
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isDesktop = event.target.innerWidth > 768;
+  }
+
 }
